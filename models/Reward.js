3b9e979bd0af1626c6d12d65441ef1718c9a8d2e
xMickeyS <xmickeys000@gmail.com>
2024-02-09 15:01:47 +0700

    update stock

--- models/Reward.js
+++ models/Reward.js
@@ -13,7 +13,7 @@ const reward_config = {
     },
     DoiKham_gift_set: {
         stars_use: 2,
-        stock: 150,
+        stock: 120,
         display: {
             name: "ของที่ระลึก",
             from: "จาก ดอยคำ",
